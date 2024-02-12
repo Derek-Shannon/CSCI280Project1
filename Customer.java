ffd6f3764ed196bec5c7f1720cd91798349995fb
Derek-Shannon <shannonderek56@gmail.com>
2024-02-12 16:04:10 -0600

    Started Work
    Lets GOOOO!

---
+++ Customer.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+/*It should include the
+developed classes (attributes and methods for each class), description of each functionality, and the
+expected output.*/
+public class Customer{
+    //name of the customer
+    String name;
+    
+    //list of accounts the user has
+    ArrayList<Account> accounts;
+
+    /* 
+     * Shows the Customer's accounts in a neat way
+    */
+    public void viewAccounts(){
+    }
+
+    /*
+     * Detetes an account with the given ID
+     * 
+     * @param ID number of the account to delete
+     */
+    public void deleteAccount(int ID){
+    }
+}
