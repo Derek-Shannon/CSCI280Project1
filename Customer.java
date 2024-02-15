75b34483a77069c73fad71256fa9b8983b08141f
Sydney <fadingstardg@gmail.com>
2024-02-15 10:59:33 -0600

    Added to Transaction + Created Statement
    Wrote constructor + toString() in Transaction
    Statement needs variables still

--- Customer.java
+++ Customer.java
@@ -4,14 +4,32 @@ public class Customer{
     private String name; //name of the customer
     private ArrayList<Account> accounts; //list of accounts the user has
 
+
+    public Customer(String name){
+        this.name = name;
+        accounts = new ArrayList<Account>();
+    }
+
     /* 
      * Shows the Customer's accounts in a neat way
     */
-    public void viewAccounts(){}
+    public void viewAccounts(){
+        
+    }
 
     /*
-     * Detetes an account with the given ID
+     * Deletes an account with the given ID
      * @param ID number of the account to delete
      */
-    public void deleteAccount(int ID){}
+    public void deleteAccount(int ID){
+
+    }
+
+    /*
+     * Deletes an account with the given ID
+     * @param ID number of the account to delete
+     */
+    public void addAccount(){
+
+    }
 }
--- Transaction.java
+++ Transaction.java
@@ -2,5 +2,18 @@ class Transaction{
     private String type; //Whether the transaction is a withdrawal, a deposit, or a transfer;
     private String memo; //Description of what the transaction was for, should the user be inclined to fill it out.
     private double amount; //the amount of moeny that was processed
-    private int date; //the time the transaction was finalized
+    private String date; //the time the transaction was finalized
+
+    // Constructor
+    public Transaction(String type, String memo, double amount, String date){
+        this.type = type;
+        this.memo = memo;
+        this.amount = amount;
+        this.date = date;
+    }
+
+    // Overwriting the toString
+    public String toString(){
+        return "" + type + " $" + amount + " " + memo + " on " + date;
+    }
 }
